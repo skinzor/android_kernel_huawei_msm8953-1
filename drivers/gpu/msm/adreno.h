6814a7b524fb2d600fc485af358b09ca74c81cc5
skinzor <robertpopescu95@yahoo.com>
2017-12-30 19:12:41 +0200

    drivers/gpu: import Huawei changes

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -862,6 +862,10 @@ extern struct adreno_gpudev adreno_a5xx_gpudev;
 extern int adreno_wake_nice;
 extern unsigned int adreno_wake_timeout;
 
+#if defined(CONFIG_HUAWEI_KERNEL_LCD) || defined(CONFIG_LCDKIT_DRIVER)
+void adreno_force_waking_gpu(void);
+#endif
+
 long adreno_ioctl(struct kgsl_device_private *dev_priv,
 		unsigned int cmd, unsigned long arg);
 
