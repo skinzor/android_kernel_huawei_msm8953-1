ab277872415f3b0db52b0140a86a80904890b109
skinzor <robertpopescu95@yahoo.com>
2018-02-04 21:22:29 +0200

    include: remove duplicate + fix build error
     CC      drivers/media/platform/soc_camera/soc_mediabus.o
      CC      drivers/media/platform/soc_camera/soc_camera_platform.o
    In file included from ../../../../../../kernel/huawei/msm8953/drivers/mmc/card/block.c:44:0:
    ../../../../../../kernel/huawei/msm8953/include/linux/mmc/card.h:145:7: error: duplicate member 'pre_eol_info'
      u8   pre_eol_info;  /* 267 */
           ^
    ../../../../../../kernel/huawei/msm8953/include/linux/mmc/card.h:146:7: error: duplicate member 'device_life_time_e
    st_typ_a'
      u8   device_life_time_est_typ_a; /* 268 */
           ^
    ../../../../../../kernel/huawei/msm8953/include/linux/mmc/card.h:147:7: error: duplicate member 'device_life_time_e
    st_typ_b'
      u8   device_life_time_est_typ_b; /* 269 */
           ^
    /home/novalineageos/LOS15/kernel/huawei/msm8953/scripts/Makefile.build:257: recipe for target 'drivers/mmc/card/blo
    ck.o' failed
    make[4]: *** [drivers/mmc/card/block.o] Error 1
    /home/novalineageos/LOS15/kernel/huawei/msm8953/scripts/Makefile.build:402: recipe for target 'drivers/mmc/card' fa
    iled
    make[3]: *** [drivers/mmc/card] Error 2
    make[3]: *** Waiting for unfinished jobs....

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -133,18 +133,15 @@ struct mmc_ext_csd {
 #define MMC_BKOPS_URGENCY_MASK 0x3
 	u8			raw_bkops_status;	/* 246 */
 	u8			raw_sectors[4];		/* 212 - 4 bytes */
-#ifdef CONFIG_HUAWEI_EMMC_DSM
-	u8			pre_eol_info;	/* 267 */
-	u8			device_life_time_est_typ_a;	/* 268 */
-	u8			device_life_time_est_typ_b;	/* 269 */
-#endif
 	u8			cmdq_depth;		/* 307 */
 	u8			cmdq_support;		/* 308 */
 	u8			barrier_support;	/* 486 */
 	u8			barrier_en;
+#ifdef CONFIG_HUAWEI_EMMC_DSM
 	u8			pre_eol_info;		/* 267 */
 	u8			device_life_time_est_typ_a;	/* 268 */
 	u8			device_life_time_est_typ_b;	/* 269 */
+#endif
 
 	u64			fw_version;		/* 254 - 8 bytes */
 	unsigned int            feature_support;
