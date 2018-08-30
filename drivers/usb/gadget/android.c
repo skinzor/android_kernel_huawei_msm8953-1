93c93e9201569445afddcf9817009151cc0dd80c
Robert Popescu <robertpopescu95@yahoo.com>
2018-08-30 11:42:27 +0300

    drivers: usb & include: linux: usb: replace with the one from the first O kernel
    * fix build errors?
    
      CC      drivers/staging/prima/CORE/HDD/src/wlan_hdd_ftm.o
      CC      drivers/staging/prima/CORE/HDD/src/wlan_hdd_hostapd.o
    ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/android.c:3315:3: error: 'ANDROID_HID' undeclared here (not in a function)
      [ANDROID_HID] = &hid_function,
       ^
    ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/android.c:3315:2: error: array index in initializer not of integer type
      [ANDROID_HID] = &hid_function,
      ^
    ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/android.c:3315:2: error: (near initialization for 'supported_functions')
    /home/novalineageos/LOS15/kernel/huawei/msm8953/scripts/Makefile.build:257: recipe for target 'drivers/usb/gadget/android.o' failed
    make[4]: *** [drivers/usb/gadget/android.o] Error 1
    make[4]: *** Waiting for unfinished jobs....
      CC      drivers/usb/serial/usb-serial.o
    In file included from ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/ci13xxx_msm.c:16:0:
    ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/ci13xxx_udc.c: In function 'udc_probe':
    ../../../../../../kernel/huawei/msm8953/drivers/usb/gadget/ci13xxx_udc.c:3907:1: warning: label 'del_udc' defined but not used [-Wunused-label]
     del_udc:
     ^

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -76,9 +76,6 @@
 #include "u_qc_ether.c"
 #include "f_gsi.c"
 #include "f_mass_storage.h"
-#include "f_hid.h"
-#include "f_hid_android_keyboard.c"
-#include "f_hid_android_mouse.c"
 
 #ifdef CONFIG_HUAWEI_USB
 #include "f_fs_hdb.c"
@@ -3109,41 +3106,6 @@ static struct android_usb_function midi_function = {
 };
 #endif
 
-static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
-{
-	return ghid_setup(cdev->gadget, 2);
-}
-
-static void hid_function_cleanup(struct android_usb_function *f)
-{
-	ghid_cleanup();
-}
-
-static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
-{
-	int ret;
-	printk(KERN_INFO "hid keyboard\n");
-	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
-		return ret;
-	}
-	printk(KERN_INFO "hid mouse\n");
-	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
-		return ret;
-	}
-	return 0;
-}
-
-static struct android_usb_function hid_function = {
-	.name		= "hid",
-	.init		= hid_function_init,
-	.cleanup	= hid_function_cleanup,
-	.bind_config	= hid_function_bind_config,
-};
-
 static int rndis_gsi_function_init(struct android_usb_function *f,
 					struct usb_composite_dev *cdev)
 {
@@ -3312,7 +3274,6 @@ static struct android_usb_function *supported_functions[] = {
 #ifdef CONFIG_SND_RAWMIDI
 	[ANDROID_MIDI] = &midi_function,
 #endif
-	[ANDROID_HID] = &hid_function,
 	[ANDROID_RNDIS_GSI] = &rndis_gsi_function,
 	[ANDROID_ECM_GSI] = &ecm_gsi_function,
 	[ANDROID_RMNET_GSI] = &rmnet_gsi_function,
@@ -3351,7 +3312,6 @@ static struct android_usb_function *default_functions[] = {
 #ifdef CONFIG_SND_RAWMIDI
 	&midi_function,
 #endif
-	&hid_function,
 	NULL
 };
 
@@ -3640,8 +3600,8 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	char buf[256], *b;
 	char aliases[256], *a;
 	int err;
+	int is_ffs;
 	int ffs_enabled = 0;
-	int hid_enabled = 0;
 
 	mutex_lock(&dev->mutex);
 
@@ -3683,22 +3643,29 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 		curr_conf = curr_conf->next;
 		while (conf_str) {
 			name = strsep(&conf_str, ",");
+			is_ffs = 0;
 			strlcpy(aliases, dev->ffs_aliases, sizeof(aliases));
 			a = aliases;
 
 			while (a) {
 				char *alias = strsep(&a, ",");
 				if (alias && !strcmp(name, alias)) {
-					name = "ffs";
+					is_ffs = 1;
 					break;
 				}
 			}
 
-			if (ffs_enabled && !strcmp(name, "ffs"))
+			if (is_ffs) {
+				if (ffs_enabled)
+					continue;
+				err = android_enable_function(dev, conf, "ffs");
+				if (err)
+					pr_err("android_usb: Cannot enable ffs (%d)",
+									err);
+				else
+					ffs_enabled = 1;
 				continue;
-
-			if (hid_enabled && !strcmp(name, "hid"))
-				continue;
+			}
 
 			if (!strcmp(name, "rndis") &&
 				!strcmp(strim(rndis_transports), "BAM2BAM_IPA") &&
@@ -3706,28 +3673,9 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 				name = "rndis_qc";
 
 			err = android_enable_function(dev, conf, name);
-			if (err) {
-				pr_err("android_usb: Cannot enable '%s' (%d)",
-							name, err);
-				continue;
-			}
-
-			if (!strcmp(name, "ffs"))
-				ffs_enabled = 1;
-
-			if (!strcmp(name, "hid"))
-				hid_enabled = 1;
-		}
-
-		/* Always enable HID gadget function. */
-		if (!hid_enabled) {
-			name = "hid";
-			err = android_enable_function(dev, conf, name);
 			if (err)
 				pr_err("android_usb: Cannot enable '%s' (%d)",
 							name, err);
-			else
-				hid_enabled = 1;
 		}
 	}
 
