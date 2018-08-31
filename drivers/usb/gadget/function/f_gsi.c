bd3b75a89916f2de42fb92b5d6c7243160513d1b
Robert Popescu <robertpopescu95@yahoo.com>
2018-08-31 19:52:10 +0300

    Revert "drivers: usb: fix build errors"
    This reverts commit 6d3fe57799178a620853d57bdded0ccde00c237a.

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -698,6 +698,9 @@ static int ipa_suspend_work_handler(struct gsi_data_port *d_port)
 	}
 	log_event_dbg("%s: Calling xdci_suspend", __func__);
 
+	ret = ipa_usb_xdci_suspend(gsi->d_port.out_channel_handle,
+				gsi->d_port.in_channel_handle, gsi->prot_id);
+
 	if (!ret) {
 		d_port->sm_state = STATE_SUSPENDED;
 		log_event_dbg("%s: STATE SUSPENDED", __func__);
